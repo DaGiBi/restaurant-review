ef424534eba356e5ea1b26f0846daa861c9a35a9
DaGiBi <najibaseri@gmail.com>
2022-09-02 18:00:47 +0800

    realm

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //used to import http method
 export default axios.create({
-  baseURL: "http://localhost:5000/api/v1/restaurant",
+  baseURL: "https://ap-southeast-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/restaurant-review-kbvvo/service/restaurant/incoming_webhook/",
   headers: {
     "Content-type": "application/json"
   }
